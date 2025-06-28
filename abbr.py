2d5d72eebfa18fe0a75c4db1ccba304b26d1a31f
qaswerg <khakovegor@icloud.com>
2025-06-28 22:04:04 +0300

    abbr.py

---
+++ abbr.py
@@ -0,0 +1,11 @@
+def abbr(frasa):
+    words_of_frasa = frasa.split()
+    abbr = ""
+    for currentword in words_of_frasa:
+        if currentword and currentword[0].isalpha():
+            abbr += currentword[0].upper()
+
+    return abbr
+
+frasa = "я люблю питон"
+print(abbr(frasa))
