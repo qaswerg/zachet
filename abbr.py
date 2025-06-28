c42f1c0e58a07167965bb911c83c3b187b6d62b3
qaswerg <khakovegor@icloud.com>
2025-06-28 22:07:04 +0300

    Update abbr.py

--- abbr.py
+++ abbr.py
@@ -4,8 +4,6 @@ def abbr(frasa):
     for currentword in words_of_frasa:
         if currentword and currentword[0].isalpha():
             abbr += currentword[0].upper()
-
     return abbr
-
 frasa = "я люблю питон"
 print(abbr(frasa))
